a0926a556deda9a536d9b8873bb8cb1d190c3bdd
Someoneeeeeeee <114275334+Someoneeeeeeee@users.noreply.github.com>
2024-09-03 20:30:02 +1000

    Added buttons for subjects and basic styling and formatting

---
+++ flash-learn/src/Homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Homepage.css';
+
+function Homepage() {
+  return (
+    <div className="container">
+      <button className="button">Mathematics</button>
+      <button className="button">Science</button>
+      <button className="button">English</button>
+    </div>
+  );
+}
+
+export default Homepage;
