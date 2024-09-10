8adc63c8849b0d121f6a347900355b36b0471fc3
Someoneeeeeeee <hfgilani06@gmail.com>
2024-09-10 23:12:28 +1000

    Updated homepage to include more buttons

--- flash-learn/src/components/HomePage/HomePage.js
+++ flash-learn/src/components/HomePage/HomePage.js
@@ -8,23 +8,46 @@ function HomePage() {
         "flex-direction": "column",
         "align-items": "center",
         "justify-content": "center"}}>
-        <NavLink to= "/English">
-            <Button colorScheme='teal' size='lg'>
-                K - 2
-            </Button>
-        </NavLink>
-        <NavLink to= "/Math">
-            <Button colorScheme='teal' size='lg'>
-                3 - 4
-            </Button>
-        </NavLink>
-        <NavLink to= "/Science">
-            <Button colorScheme='teal' size='lg'>
-                5 - 6
-            </Button>
-        </NavLink>
+        <div>
+            <NavLink to= "/English">
+                <Button colorScheme='teal' size='lg'>
+                    English
+                </Button>
+            </NavLink>
+            <NavLink to= "/Math">
+                <Button colorScheme='teal' size='lg'>
+                    Maths
+                </Button>
+            </NavLink>
+            <NavLink to= "/Science">
+                <Button colorScheme='teal' size='lg'>
+                    Science
+                </Button>
+            </NavLink>
+        </div>
+        <div>
+        <NavLink to= "/easy">
+                <Button colorScheme='teal' size='lg'>
+                    K - 2
+                </Button>
+            </NavLink>
+            <NavLink to= "/moderate">
+                <Button colorScheme='teal' size='lg'>
+                    3 - 4
+                </Button>
+            </NavLink>
+            <NavLink to= "/hard">
+                <Button colorScheme='teal' size='lg'>
+                    5 - 6
+                </Button>
+            </NavLink>
+        </div>
     </div>
     );
 }
 
 export default HomePage;
+
+//TODO: 
+// format the levels to display at the bottom and be highlighted when selected
+// Format the subjects so it is in the center on the page
