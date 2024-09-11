75577ec8e50ca31c5c65b2818f8707a2bf73cf33
vSzak <szakacsalexander@gmail.com>
2024-09-11 12:37:22 +1000

    Changes

---
+++ flash-learn/src/components/EnglishPage/SpellingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SpellingPage() {
+    console.log("SpellingPage loaded");
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>This is the Spelling Page</h1>
+        </div>
+    );
+}
+
+export default SpellingPage;
