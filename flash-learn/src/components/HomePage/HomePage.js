d9fffb8b66f4091f375da4731846cabb23b93b41
SheereenSA <sheereen.sidiqali@gmail.com>
2024-09-16 23:26:42 +1000

    Fixed selection of year group

--- flash-learn/src/components/HomePage/HomePage.js
+++ flash-learn/src/components/HomePage/HomePage.js
@@ -1,29 +1,37 @@
-import { Button } from "@chakra-ui/react"
+import { Button, Text } from "@chakra-ui/react"
 import { NavLink } from 'react-router-dom'
 
 function HomePage() {
-    return(
-    <div style={
-        {"display": "flex",
-        "flex-direction": "column",
-        "align-items": "center",
-        "justify-content": "center"}}>
-        <NavLink to= "/English">
-            <Button colorScheme='teal' size='lg'>
-                K - 2
-            </Button>
-        </NavLink>
-        <NavLink to= "/Math">
-            <Button colorScheme='teal' size='lg'>
-                3 - 4
-            </Button>
-        </NavLink>
-        <NavLink to= "/Science">
-            <Button colorScheme='teal' size='lg'>
-                5 - 6
-            </Button>
-        </NavLink>
-    </div>
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",  
+            alignItems: "center",    
+            justifyContent: "center", 
+            height: "100vh",          
+            gap: "60px"               
+        }}>
+            {/* Title */}
+            <Text fontSize="3xl" mb="20px" fontWeight="bold"> 
+                Select your year group
+            </Text>
+
+            <NavLink to="/English">
+                <Button colorScheme="teal" size="6xl" fontSize="4xl" w="800px" h="100px" >
+                    K - 2
+                </Button>
+            </NavLink>
+            <NavLink to="/Math">
+                <Button colorScheme="teal" size="6xl" fontSize="4xl" w="800px" h="100px" >
+                    3 - 4
+                </Button>
+            </NavLink>
+            <NavLink to="/Science">
+                <Button colorScheme="teal" size="6xl" fontSize="4xl" w="800px" h="100px" >
+                    5 - 6
+                </Button>
+            </NavLink>
+        </div>
     );
 }
 
